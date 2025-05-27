336eb96a4d137f86c54b05ea5fe2c8c2a74216ee
nick <nschmid8@hs-mittweida.de>
2025-05-27 22:25:23 +0200

    first commit

---
+++ lua/config/lazy.lua
@@ -0,0 +1,62 @@
+-- setup lazy.nvim local 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- colorscheme
+vim.cmd.colorscheme("zenwritten")
+vim.opt.background = "dark"
+vim.g.zenbones = {
+    lightness = "warm"
+}
+-- some useful vim options 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4 -- auto indent
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split" -- e.g. in :%s...
+
+vim.opt.termguicolors = true
+
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+
+     "sainnhe/gruvbox-material",
+     "junegunn/seoul256.vim",
+     "rktjmp/lush.nvim",
+     "zenbones-theme/zenbones.nvim",
+}
