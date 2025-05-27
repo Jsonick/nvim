d1b9a88e92fe30f5dcb8988ae7699bcdafd98161
nick <nick.schmidt1711@gmail.com>
2025-05-28 00:53:47 +0200

    update dependencies

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,32 +31,3 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
--- colorscheme
-vim.cmd.colorscheme("zenwritten")
-vim.opt.background = "dark"
-vim.g.zenbones = {
-    lightness = "warm"
-}
--- some useful vim options 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.wrap = false
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4 -- auto indent
-
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.scrolloff = 999
-
-vim.opt.virtualedit = "block"
-
-vim.opt.inccommand = "split" -- e.g. in :%s...
-
-vim.opt.termguicolors = true
-
