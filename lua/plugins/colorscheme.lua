5a474fe21a9e06d6987e9e69b1869540796fe793
nick <nick.schmidt1711@gmail.com>
2025-05-28 00:51:58 +0200

    dark-metal colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,24 @@
 return {
-
-     "sainnhe/gruvbox-material",
-     "junegunn/seoul256.vim",
-     "rktjmp/lush.nvim",
-     "zenbones-theme/zenbones.nvim",
+    --"junegunn/seoul256.vim",
+   
+    "metalelf0/black-metal-theme-neovim",
+    
+    config = function()
+        vim.o.background = "dark"
+        vim.cmd.colorscheme("burzum-alt")
+    end,
+    
+    --"zenbones-theme/zenbones.nvim", -- if changing colorscheme, 
+                                    -- dont forget to change or comment the config
+    --config = function()    
+    --    vim.opt.termguicolors = true
+    --    vim.opt.background = "dark"
+    --    vim.g.zenbones = {
+    --        lightness = "warm"
+    --    }
+    --    vim.cmd.colorscheme("zenwritten")
+    --end,
+    --dependencies = {
+    --    "rktjmp/lush.nvim",
+    --}
 }
